c3d945b9edc4d250bf75a7baf63c838980c45662
Ewald van Veen <ewaldvanveen@gmail.com>
2020-08-04 17:26:51 +0200

    Check to see if source control in VS Code is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tp';
+  title = 'Team Planner 2020 | By Ewald van Veen';
 }
