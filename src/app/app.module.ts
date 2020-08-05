94506a17e5a05e9f45d04d049dab2613dd69dcf5
Ewald van Veen <ewaldvanveen@gmail.com>
2020-08-05 16:04:56 +0200

    Added the basic structure of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { FeaturesComponent } from './features/features.component';
+import { CtaComponent } from './cta/cta.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    CtaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
